2367041f06ad21dd4b6da9651f9bb301fd438f0f
Simon Kroeger <simon.kroeger@sponsorpay.com>
2014-08-04 16:59:17 +0200

    immutable board + using app trait

--- sudoku.scala
+++ sudoku.scala
@@ -1,36 +1,25 @@
-import org.msgpack._
+object Sudoku extends App {
+  type Board = Vector[Int]
 
-class Sudoku(source: String) {
-  val board = source.toList.filter("123456789_." contains _).map(_.asDigit).toArray
+  def solve(board: Board = board, index: Int = 0): Option[Board] = {
+    def guess(number: Int) = {
+      def taken = neighbours(index).exists(board(_) == number)
 
-  def solve(index: Int = 0): Boolean = {
-    def guess(number: Int): Boolean = {
-      val f = board(index)
-      if (f > 0 && f != number || taken(index, number)) return false
-      board(index) = number
-      if (index == 80 || solve(index + 1)) return true
-      board(index) = f
-      false
+      if (index > 80) Some(board)
+      else if (board(index) > 0 && board(index) != number || taken) None
+      else solve(board.updated(index, number), index + 1)
     }
-    1 to 9 exists guess
+
+    (1 to 9).view.flatMap(guess).headOption
   }
 
-  private val neighbours = (0 to 80).map(i => (0 to 80).filter(j => neighbour(i, j)))
-
-  private def neighbour(i: Int, j: Int): Boolean = {
+  def neighbour(i: Int, j: Int) = {
     def box(n: Int) = n / 27 * 3 + n % 9 / 3
     (i != j) && (i / 9 == j / 9 || i % 9 == j % 9 || box(i) == box(j))
   }
 
-  private def taken(index: Int, number: Int): Boolean = {
-    neighbours(index).exists(board(_) == number)
-  }
-}
+  val neighbours = (0 to 80).map(i => (0 to 80).filter(j => neighbour(i, j)))
 
-object Sudoku {
-  def main(args: Array[String]) {
-    val game = new Sudoku(readLine)
-    game.solve()
-    game.board.grouped(9).foreach(row => println(row.mkString(" ")))
-  }
+  def board = readLine.filter("123456789_." contains _).map(_.asDigit).toVector
+  solve(board).foreach(_.grouped(9).foreach(row => println(row.mkString(" "))))
 }
