9d02ccc9793fbc7118845dad0f16f26a113e1a03
Simon Kroeger <simon.kroeger@sponsorpay.com>
2014-10-17 13:32:32 +0200

    adding the board

--- build.sbt
+++ build.sbt
@@ -1,5 +1,14 @@
+libraryDependencies += "org.msgpack" %% "msgpack-scala" % "0.6.8"
+
+libraryDependencies += "org.apache.kafka" % "kafka_2.9.2" % "0.8.1.1" exclude("com.sun.jmx","jmxri") exclude("com.sun.jdmk","jmxtools") exclude("javax.jms","jms")
+
+resolvers += "Apache repo" at "https://repository.apache.org/content/repositories/releases" 
+
 name := "sudoku"
 
 version := "1.0"
 
 scalaVersion := "2.10.4"
+
+mainClass in (Compile, run) := Some("Sudoku")
+
--- sudoku.scala
+++ sudoku.scala
@@ -1,25 +1,30 @@
 object Sudoku extends App {
   type Board = Vector[Int]
 
-  def solve(board: Board = board, index: Int = 0): Option[Board] = {
-    def guess(number: Int) = {
-      def taken = neighbours(index).exists(board(_) == number)
+  def neighbour(i: Int, j: Int): Boolean = {
+    def box(n: Int) = (n / 27 * 3) + (n % 9) / 3 
+    (i != j) && (i % 9 == j % 9 || i / 9 == j / 9 || box(i) == box(j))
+  }
 
+  val neighbours = (0 to 80).map(i =>
+    (0 to 80).filter(j => neighbour(i, j))
+  )
+
+  def solve(board: Board, index: Int = 0): Option[Board] = {
+    def taken(number: Int): Boolean = 
+      neighbours(index).exists(n => board(n) == number)
+    
+    def guess(number: Int): Option[Board] = 
       if (index > 80) Some(board)
-      else if (board(index) > 0 && board(index) != number || taken) None
+      else if (board(index) > 0 && board(index) != number || taken(number)) None
       else solve(board.updated(index, number), index + 1)
-    }
-
-    (1 to 9).view.flatMap(guess).headOption
+    
+    (1 to 9).view.flatMap(n => guess(n)).headOption
   }
 
-  def neighbour(i: Int, j: Int) = {
-    def box(n: Int) = n / 27 * 3 + n % 9 / 3
-    (i != j) && (i / 9 == j / 9 || i % 9 == j % 9 || box(i) == box(j))
-  }
+  val puzzle = "............942.8.16.....29........89.6.....14..25......4.......2...8.9..5....7.."
+  val board = puzzle.map(c => c.asDigit).toVector
+  val solved = solve(board).get
 
-  val neighbours = (0 to 80).map(i => (0 to 80).filter(j => neighbour(i, j)))
-
-  def board = readLine.filter("123456789_." contains _).map(_.asDigit).toVector
-  solve(board).foreach(_.grouped(9).foreach(row => println(row.mkString(" "))))
+  solved.grouped(9).foreach(row => println(row.mkString(" ")))
 }
